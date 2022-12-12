7b69d06cda60d417195fb223a49e42d1e3070d3e
Морозевич <romamorozevich@yandex.ru>
2022-12-12 23:46:42 +0300

    question

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,28 +1,39 @@
 from rest_framework import serializers
 
+from logistic.models import Product, Stock, StockProduct
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
     pass
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id','address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
         # создаем склад по его параметрам
         stock = super().create(validated_data)
 
+        for position in positions:
+            position['stock'] = stock
+            StockProduct.objects.create(**position)
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
@@ -35,6 +46,11 @@ class StockSerializer(serializers.ModelSerializer):
 
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
+
+        for position in positions:
+            position['stock'] = stock
+            obj, created = StockProduct.objects.update_or_create(product=position['product'],quantity=position['quantity'],price=position['price'], defaults=position)
+
 
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -80,9 +80,11 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_stocks_products',
+        'NAME': 'crud',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'Roma2003',
     }
 }
 
